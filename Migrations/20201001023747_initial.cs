6333f3c53aca6ddd4382a3c8e83d378f4bd6fdc8
Adrian Alcides Mendez Vargas <adrian_mendez@ucne.edu.do>
2020-09-30 22:42:33 -0400

    Agregando migracion e icono al navMenu

---
+++ Migrations/20201001023747_initial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Adrian_P1_AP2.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TipoClientes",
+                columns: table => new
+                {
+                    TipoClienteId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoClientes", x => x.TipoClienteId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TipoClientes");
+        }
+    }
+}
